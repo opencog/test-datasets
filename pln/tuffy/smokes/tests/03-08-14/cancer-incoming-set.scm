8ab5928323a65549d96c204d34b044de02378c98
Cosmo Harrigan <cosmo.harrigan@singularityu.org>
2014-03-09 18:35:44 -0700

    Add new output after updating deduction formula

---
+++ pln/tuffy/smokes/tests/03-08-14/cancer-incoming-set.scm
@@ -0,0 +1,152 @@
+[0;32mopencog[1;32m> [0mEntering scheme shell; use ^D or a single . on a line by itself to exit.
+[0;34mguile[1;34m> [0m((EvaluationLink
+   (ConceptNode "Gary" (stv -nan 1))
+   (PredicateNode "cancer" (av 1000 0 0))
+)
+ (EvaluationLink (stv -nan 1)
+   (PredicateNode "cancer" (av 1000 0 0))
+   (ConceptNode "Gary" (stv -nan 1))
+)
+ (EvaluationLink
+   (ListLink
+      (ConceptNode "Gary" (stv -nan 1))
+   )
+   (PredicateNode "cancer" (av 1000 0 0))
+)
+ (EvaluationLink
+   (ConceptNode "Bob" (stv -nan 1))
+   (PredicateNode "cancer" (av 1000 0 0))
+)
+ (EvaluationLink (stv -nan 1)
+   (PredicateNode "cancer" (av 1000 0 0))
+   (ConceptNode "Bob" (stv -nan 1))
+)
+ (EvaluationLink
+   (ListLink
+      (ConceptNode "Bob" (stv -nan 1))
+   )
+   (PredicateNode "cancer" (av 1000 0 0))
+)
+ (EvaluationLink (stv -nan 1)
+   (PredicateNode "cancer" (av 1000 0 0))
+   (ConceptNode "Frank" (stv -nan 1))
+)
+ (EvaluationLink
+   (ConceptNode "Frank" (stv -nan 1))
+   (PredicateNode "cancer" (av 1000 0 0))
+)
+ (EvaluationLink
+   (ConceptNode "Helen" (stv -nan 1))
+   (PredicateNode "cancer" (av 1000 0 0))
+)
+ (EvaluationLink
+   (ListLink
+      (ConceptNode "Frank" (stv -nan 1))
+   )
+   (PredicateNode "cancer" (av 1000 0 0))
+)
+ (EvaluationLink (stv -nan 1)
+   (PredicateNode "cancer" (av 1000 0 0))
+   (ConceptNode "Helen" (stv -nan 1))
+)
+ (EvaluationLink (stv -nan 0.99999994)
+   (PredicateNode "cancer" (av 1000 0 0))
+   (ListLink
+      (ConceptNode "Bob" (stv -nan 1))
+   )
+)
+ (EvaluationLink
+   (ConceptNode "smokes" (stv -nan -nan))
+   (PredicateNode "cancer" (av 1000 0 0))
+)
+ (EvaluationLink (stv -nan -nan)
+   (PredicateNode "cancer" (av 1000 0 0))
+   (ConceptNode "Edward" (stv -nan -nan))
+)
+ (EvaluationLink (stv -nan -nan)
+   (PredicateNode "cancer" (av 1000 0 0))
+   (ConceptNode "smokes" (stv -nan -nan))
+)
+ (EvaluationLink
+   (ListLink
+      (VariableNode "$X")
+   )
+   (PredicateNode "cancer" (av 1000 0 0))
+)
+ (EvaluationLink (stv -nan 0.99999994)
+   (PredicateNode "cancer" (av 1000 0 0))
+   (ListLink
+      (ConceptNode "Frank" (stv -nan 1))
+   )
+)
+ (EvaluationLink
+   (ConceptNode "Anna" (stv -nan -nan))
+   (PredicateNode "cancer" (av 1000 0 0))
+)
+ (EvaluationLink (stv -nan -nan)
+   (PredicateNode "cancer" (av 1000 0 0))
+   (ConceptNode "Anna" (stv -nan -nan))
+)
+ (EvaluationLink
+   (ListLink
+      (ConceptNode "Edward" (stv -nan -nan))
+   )
+   (PredicateNode "cancer" (av 1000 0 0))
+)
+ (EvaluationLink
+   (ConceptNode "Edward" (stv -nan -nan))
+   (PredicateNode "cancer" (av 1000 0 0))
+)
+ (EvaluationLink (stv -nan 1)
+   (PredicateNode "cancer" (av 1000 0 0))
+   (ListLink
+      (ConceptNode "Anna" (stv -nan -nan))
+   )
+)
+ (EvaluationLink (stv -nan 1)
+   (PredicateNode "cancer" (av 1000 0 0))
+   (ListLink
+      (ConceptNode "Edward" (stv -nan -nan))
+   )
+)
+ (EvaluationLink (stv 1 0.99999982)
+   (PredicateNode "cancer" (av 1000 0 0))
+   (ListLink
+      (VariableNode "$X")
+   )
+)
+ (EvaluationLink
+   (PredicateNode "cancer" (av 1000 0 0))
+   (ListLink
+      (VariableNode "$Y")
+   )
+)
+ (NotLink
+   (PredicateNode "cancer" (av 1000 0 0))
+)
+ (EvaluationLink
+   (ListLink
+      (ConceptNode "Anna" (stv -nan -nan))
+   )
+   (PredicateNode "cancer" (av 1000 0 0))
+)
+ (EvaluationLink (stv -nan 0.0012484394)
+   (PredicateNode "cancer" (av 1000 0 0))
+   (ListLink
+      (ConceptNode "Gary" (stv -nan 1))
+   )
+)
+ (EvaluationLink
+   (PredicateNode "cancer" (av 1000 0 0))
+   (ListLink
+      (ConceptNode "Helen" (stv -nan 1))
+   )
+)
+ (EvaluationLink
+   (PredicateNode "cancer" (av 1000 0 0))
+   (ListLink
+      (VariableNode "$hasCancer")
+   )
+)
+)
+[0;34mguile[1;34m> [0m
---
+++ pln/tuffy/smokes/tests/03-08-14/smokes-incoming-set.scm
@@ -0,0 +1,170 @@
+[0;32mopencog[1;32m> [0mEntering scheme shell; use ^D or a single . on a line by itself to exit.
+[0;34mguile[1;34m> [0m((EvaluationLink
+   (PredicateNode "smokes")
+   (ListLink
+      (EvaluationLink (stv -nan -nan)
+         (PredicateNode "friends")
+         (ListLink
+            (ConceptNode "Frank" (stv -nan 1))
+            (ConceptNode "Bob" (stv -nan 1))
+         )
+      )
+   )
+)
+ (EvaluationLink
+   (ConceptNode "Bob" (stv -nan 1))
+   (PredicateNode "smokes")
+)
+ (EvaluationLink
+   (ConceptNode "Gary" (stv -nan 1))
+   (PredicateNode "smokes")
+)
+ (EvaluationLink (stv -nan 1)
+   (PredicateNode "smokes")
+   (ConceptNode "Bob" (stv -nan 1))
+)
+ (EvaluationLink (stv -nan 1)
+   (PredicateNode "smokes")
+   (ConceptNode "Gary" (stv -nan 1))
+)
+ (EvaluationLink
+   (ConceptNode "Frank" (stv -nan 1))
+   (PredicateNode "smokes")
+)
+ (EvaluationLink (stv -nan 1)
+   (PredicateNode "smokes")
+   (ConceptNode "Frank" (stv -nan 1))
+)
+ (EvaluationLink
+   (ListLink
+      (ConceptNode "Helen" (stv -nan 1))
+   )
+   (PredicateNode "smokes")
+)
+ (EvaluationLink
+   (ConceptNode "Helen" (stv -nan 1))
+   (PredicateNode "smokes")
+)
+ (EvaluationLink (stv -nan 1)
+   (PredicateNode "smokes")
+   (ConceptNode "Helen" (stv -nan 1))
+)
+ (EvaluationLink
+   (ListLink
+      (ConceptNode "Frank" (stv -nan 1))
+   )
+   (PredicateNode "smokes")
+)
+ (EvaluationLink
+   (ListLink
+      (ConceptNode "Gary" (stv -nan 1))
+   )
+   (PredicateNode "smokes")
+)
+ (EvaluationLink
+   (ListLink
+      (ConceptNode "Bob" (stv -nan 1))
+   )
+   (PredicateNode "smokes")
+)
+ (EvaluationLink
+   (ConceptNode "Anna" (stv -nan -nan))
+   (PredicateNode "smokes")
+)
+ (EvaluationLink
+   (ConceptNode "Edward" (stv -nan -nan))
+   (PredicateNode "smokes")
+)
+ (EvaluationLink
+   (ConceptNode "cancer" (stv -nan -nan))
+   (PredicateNode "smokes")
+)
+ (EvaluationLink (stv -nan -nan)
+   (PredicateNode "smokes")
+   (ConceptNode "Anna" (stv -nan -nan))
+)
+ (EvaluationLink (stv -nan -nan)
+   (PredicateNode "smokes")
+   (ConceptNode "cancer" (stv -nan -nan))
+)
+ (EvaluationLink
+   (ListLink
+      (VariableNode "$Y")
+   )
+   (PredicateNode "smokes")
+)
+ (EvaluationLink (stv -nan -nan)
+   (PredicateNode "smokes")
+   (ConceptNode "Edward" (stv -nan -nan))
+)
+ (EvaluationLink
+   (ListLink
+      (ConceptNode "Anna" (stv -nan -nan))
+   )
+   (PredicateNode "smokes")
+)
+ (EvaluationLink (stv 1 0.99999982)
+   (PredicateNode "smokes")
+   (ListLink
+      (VariableNode "$Y")
+   )
+)
+ (EvaluationLink (stv 1 0.99999982)
+   (PredicateNode "smokes")
+   (ListLink
+      (VariableNode "$X")
+   )
+)
+ (EvaluationLink (stv -nan 1)
+   (PredicateNode "smokes")
+   (ListLink
+      (ConceptNode "Helen" (stv -nan 1))
+   )
+)
+ (EvaluationLink (stv -nan 1)
+   (PredicateNode "smokes")
+   (ListLink
+      (ConceptNode "Bob" (stv -nan 1))
+   )
+)
+ (EvaluationLink (stv -nan 1)
+   (PredicateNode "smokes")
+   (ListLink
+      (ConceptNode "Gary" (stv -nan 1))
+   )
+)
+ (EvaluationLink (stv -nan 1)
+   (PredicateNode "smokes")
+   (ListLink
+      (ConceptNode "Frank" (stv -nan 1))
+   )
+)
+ (EvaluationLink
+   (ListLink
+      (VariableNode "$X")
+   )
+   (PredicateNode "smokes")
+)
+ (EvaluationLink
+   (ListLink
+      (ConceptNode "Edward" (stv -nan -nan))
+   )
+   (PredicateNode "smokes")
+)
+ (NotLink
+   (PredicateNode "smokes")
+)
+ (EvaluationLink (stv -nan 1)
+   (PredicateNode "smokes")
+   (ListLink
+      (ConceptNode "Edward" (stv -nan -nan))
+   )
+)
+ (EvaluationLink (stv -nan 1)
+   (PredicateNode "smokes")
+   (ListLink
+      (ConceptNode "Anna" (stv -nan -nan))
+   )
+)
+)
+[0;34mguile[1;34m> [0m
