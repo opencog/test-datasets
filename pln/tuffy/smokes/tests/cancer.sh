d9ca52edda3628e16d9fb6999c52f99ec70f9817
Cosmo Harrigan <cosmo.harrigan@singularityu.org>
2014-03-09 18:35:24 -0700

    Add test scripts for tuffy example

---
+++ pln/tuffy/smokes/tests/cancer.sh
@@ -0,0 +1,2 @@
+../oc.sh "(cog-incoming-set cancer)" > cancer-incoming-set.scm
+more cancer-incoming-set.scm
---
+++ pln/tuffy/smokes/tests/friends.sh
@@ -0,0 +1,2 @@
+../oc.sh "(cog-incoming-set friends)" > friends-incoming-set.scm
+more friends-incoming-set.scm
