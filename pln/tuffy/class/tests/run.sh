2398f7ae51cdd11610a2a8704691dc64baa87ad6
Cosmo Harrigan <cosmo.harrigan@singularityu.org>
2014-03-24 22:20:31 -0700

    Add automated test procedure for tuffy 'class' experiment

---
+++ pln/tuffy/class/tests/run.sh
@@ -0,0 +1,5 @@
+../oc.sh agents-start ImportanceUpdatingAgent
+../oc.sh agents-start ImportanceDiffusionAgent
+../oc.sh agents-start HebbianUpdatingAgent
+../oc.sh loadpy class_agent
+../oc.sh agents-start class_agent.InferenceAgent
